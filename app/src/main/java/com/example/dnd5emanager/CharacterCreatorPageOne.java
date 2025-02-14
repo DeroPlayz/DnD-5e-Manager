54cb2bb59c073b37c5c93518c039cffc04a915e6
matthew.ahwal <deroplayz@gmail.com>
2025-02-14 13:51:45 -0800

    shit dude buttons are hard

--- app/src/main/java/com/example/dnd5emanager/CharacterCreatorPageOne.java
+++ app/src/main/java/com/example/dnd5emanager/CharacterCreatorPageOne.java
@@ -1,15 +1,13 @@
 package com.example.dnd5emanager;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.dnd5emanager.databinding.CharacterViewBinding;
 
@@ -18,7 +16,7 @@ import com.example.dnd5emanager.databinding.CharacterViewBinding;
 // * Use the {@link ThirdFragment#newInstance} factory method to
 // * create an instance of this fragment.
 // */
-public class CharacterView extends Fragment {
+public class CharacterCreatorPageOne extends Fragment {
     private CharacterViewBinding binding;
 
     @Override
