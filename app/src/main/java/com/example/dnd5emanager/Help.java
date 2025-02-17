d88f3242307caec1fa54cc4cdc3a92723fa2dc29
DeroPlayz <deroplayz@gmail.com>
2025-02-17 10:41:22 -0800

    Changes & Optimizations! - Tweaked character_creator_page_one.xml - Renamed dm_manager.xml, DMManager.java, Help.java, Settings.java, settings.xml, and fixed their references so they reference the current name.

--- app/src/main/java/com/example/dnd5emanager/Help.java
+++ app/src/main/java/com/example/dnd5emanager/Help.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DmManger#newInstance} factory method to
+ * Use the {@link Help#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DmManger extends Fragment {
+public class Help extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class DmManger extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DmManger() {
+    public Help() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class DmManger extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DmManger.
+     * @return A new instance of fragment help.
      */
     // TODO: Rename and change types and number of parameters
-    public static DmManger newInstance(String param1, String param2) {
-        DmManger fragment = new DmManger();
+    public static Help newInstance(String param1, String param2) {
+        Help fragment = new Help();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class DmManger extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dm_manger, container, false);
+        return inflater.inflate(R.layout.help, container, false);
     }
 }
