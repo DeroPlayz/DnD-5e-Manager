838e4dbc24d0f9218753f6711a8a5fdf7685254a
Alexis <ambrizlex@gmail.com>
2025-02-27 13:43:31 -0800

    dm player manager  button & page completed
    Signed-off-by: Alexis <ambrizlex@gmail.com>

---
+++ app/src/main/java/com/example/dnd5emanager/DmPlayerManager.java
@@ -0,0 +1,53 @@
+package com.example.dnd5emanager;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dnd5emanager.DataClasses.PlayerCharacter;
+import com.example.dnd5emanager.databinding.DmPlayerManagerBinding;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DmPlayerManager extends Fragment {
+
+    private DmPlayerManagerBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = DmPlayerManagerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.toSheet.setOnClickListener(v ->
+                NavHostFragment.findNavController(DmPlayerManager.this).navigate(R.id.goToMainMenu));
+        binding.dpmToMain.setOnClickListener(v ->
+                NavHostFragment.findNavController(DmPlayerManager.this).navigate(R.id.action_DMManager_to_campaignList));
+
+    }
+
+    public static ArrayList<PlayerCharacter> Characters = new ArrayList<PlayerCharacter>(List.of(new PlayerCharacter()));
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
