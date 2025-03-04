2ce85979620d02833140ed07abee6b8deaad2851
Alexis <ambrizlex@gmail.com>
2025-03-04 13:55:18 -0800

    main menu done app has a bug! merge at your own risk
    Signed-off-by: Alexis <ambrizlex@gmail.com>

--- app/src/main/java/com/example/dnd5emanager/MainMenu.java
+++ app/src/main/java/com/example/dnd5emanager/MainMenu.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -48,6 +49,17 @@ public class MainMenu extends Fragment {
         binding.mainMenuViewDMMenu.setOnClickListener(v ->
                 NavHostFragment.findNavController(MainMenu.this).navigate(R.id.goToDMManager)
         );
+        Button mainMenuViewCharacters = view.findViewById(R.id.main_menu_view_characters);
+        mainMenuViewCharacters.setTextSize(45);
+        Button mainMenuViewDMMenu = view.findViewById(R.id.main_menu_view_d_m_menu);
+        mainMenuViewDMMenu.setTextSize(45);
+        Button mainMenuViewData = view.findViewById(R.id.main_menu_view_data);
+        mainMenuViewData.setTextSize(45);
+        Button mainMenuSettings = view.findViewById(R.id.main_menu_settings);
+        mainMenuSettings.setTextSize(45);
+        Button mainMenuHelpPage = view.findViewById(R.id.main_menu_help_page);
+        mainMenuHelpPage.setTextSize(45);
+
     }
 
     public static ArrayList<PlayerCharacter> Characters = new ArrayList<PlayerCharacter>(List.of(new PlayerCharacter()));
