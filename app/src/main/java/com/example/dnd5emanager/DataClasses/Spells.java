068fb9b028c60384229ed1d2d8de0ba31808bb1d
Matthew Ahwal <deroplayz@gmail.com>
2025-03-07 13:54:12 -0800

    poosh!

--- app/src/main/java/com/example/dnd5emanager/DataClasses/Spells.java
+++ app/src/main/java/com/example/dnd5emanager/DataClasses/Spells.java
@@ -4,4 +4,12 @@ public class Spells {
     String Element;
     int Level;
     String School;
+    String Description;
+
+    public Spells(String Element, int Level, String School, String Description) {
+        this.Element = Element;
+        this.Level = Level;
+        this.School = School;
+        this.Description = Description;
+    }
 }
