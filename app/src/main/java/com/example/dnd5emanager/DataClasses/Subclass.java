e36568a3675d619239ae9ace7f635265e17fb8c7
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2025-03-20 18:13:40 +0000

    lots of formatting fixes!

--- app/src/main/java/com/example/dnd5emanager/DataClasses/Subclass.java
+++ app/src/main/java/com/example/dnd5emanager/DataClasses/Subclass.java
@@ -3,30 +3,7 @@ package com.example.dnd5emanager.DataClasses;
 import java.util.ArrayList;
 
 public class Subclass{
-
-    private CharacterClass Parent;
-        public CharacterClass getParent(){return Parent;}
-
-    private String Name = "";
-        public String getName() {
-            if (Name == null){
-                return Name;
-            }
-            return Parent.getName();
-        }
-
-    private ArrayList<Feature> SubclassFeatures = null;
-        public ArrayList<Feature> getFeatures(){return SubclassFeatures;}
-
-    public Subclass(String Name, CharacterClass Parent){
-        this.Name = Name;
-        this.Parent = Parent;
-    }
-
-    public Subclass(String Name, CharacterClass Parent, ArrayList<Feature> SubclassFeatures){
-        this.Name = Name;
-        this.Parent = Parent;
-        this.SubclassFeatures = SubclassFeatures;
-    }
-    public Subclass(){}
+    String Name;
+    String ACMod;
+    
 }
