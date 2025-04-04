001ae09d54fa29abe1690e5362378c18228bf4a7
Alexis <ambrizlex@gmail.com>
2025-04-04 13:52:41 -0700

    formated DM_Manger like 5e Companion App. No code yet just text boxes Todo - enable save function; hide text boxes until user asks for them ;
    Signed-off-by: Alexis <ambrizlex@gmail.com>

--- app/src/main/java/com/example/dnd5emanager/CharacterView.java
+++ app/src/main/java/com/example/dnd5emanager/CharacterView.java
@@ -133,9 +133,8 @@ public class CharacterView extends Fragment {
         binding.characterViewBackButton.setOnClickListener(v -> {
             NavHostFragment.findNavController(CharacterView.this).navigate(R.id.goToCharacterList);
         });
-        binding.toDice.setOnClickListener(v -> {
-            NavHostFragment.findNavController(CharacterView.this).navigate(R.id.goToDiceRoller);
-        });
+        binding.toDice.setOnClickListener(v ->
+                NavHostFragment.findNavController(CharacterView.this).navigate(R.id.goToDiceRoller));
 
         binding.characterViewDeleteCharacter.setOnClickListener(v -> {
             NavHostFragment.findNavController(CharacterView.this).navigate(R.id.goToCharacterList);
