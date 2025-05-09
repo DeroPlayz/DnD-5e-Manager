683d92f2767e260da5fc632dc26e40d1b7a1c625
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2025-05-09 16:51:25 +0000

    compooter

--- app/src/main/java/com/example/dnd5emanager/DataClasses/Weapon.java
+++ app/src/main/java/com/example/dnd5emanager/DataClasses/Weapon.java
@@ -5,14 +5,20 @@ import java.util.Map;
 
 public class Weapon {
     private String Name;
-    private String AmmunitionType;
+    private String Description;
+
+    private int AttackBonus;
+
     private int DamageDice;
     private int DiceCount;
     private String DamageType;
-    private String Description;
-    private int ExtraAttackBonus;
-    private int ExtraDamageBonus;
-    private final Map<String, Integer> ExtraDamageDice = new HashMap<>();
+    private final Map<String, Integer> ExtraDamageDice = new HashMap<>(){{
+        this.put("D4", 0);
+        this.put("D6", 0);
+        this.put("D8", 0);
+        this.put("D10", 0);
+        this.put("D20", 0);
+    }};
 
     private boolean IsSimple;
     private boolean IsVersatile;
@@ -34,4 +40,38 @@ public class Weapon {
     private boolean IsRanged;
     private boolean IsLoading;
     private boolean IsThrown;
+    private String AmmunitionType;
+    
+    public Weapon(String Name, String Description,
+        int AttackBonus,
+        int DamageDice, int DiceCount, String DamageType,
+        boolean isSimple, boolean IsVersatile, boolean isFinesse,
+        boolean isLight, boolean isHeavy,
+        boolean isSilver, boolean isTwoHanded,
+        boolean isAttuned, boolean isSpecial, boolean isCustom, boolean IsImprovised,
+        boolean hasReach, boolean isRanged, boolean isLoading, boolean IsThrown, String AmmunitionType
+    ){
+        this.Name = Name;
+        this.Description = Description;
+        this.AttackBonus = AttackBonus;
+        this.DamageDice = DamageDice;
+        this.DiceCount = DiceCount;
+        this.DamageType = DamageType;
+        this.isSimple = isSimple;
+        this.isVersatile = isVersatile;
+        this.isFinesse = isFinesse;
+        this.isLight = isLight;
+        this.isHeavy = isHeavy;
+        this.isSilver = isSilver;
+        this.isTwoHanded = isTwoHanded;
+        this.isAttuned = isAttuned;
+        this.isSpecial = isSpecial;
+        this.isCustom = isCustom;
+        this.IsImprovised = isImprovised;
+        this.hasReach = hasReach;
+        this.isRanged = isRanged;
+        this.isLoading = IsLoading;
+        this.IsThrown = isThrown;
+        this.AmmunitionType = AmmunitionType;
+    }
 }
