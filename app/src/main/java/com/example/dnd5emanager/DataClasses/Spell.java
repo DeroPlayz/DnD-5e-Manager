e73cb729e5c3b1ae7dbe7eee2b43bff1714d9c40
matthew.ahwal <deroplayz@gmail.com>
2025-05-09 13:47:25 -0700

    spell compendium works!

--- app/src/main/java/com/example/dnd5emanager/DataClasses/Spell.java
+++ app/src/main/java/com/example/dnd5emanager/DataClasses/Spell.java
@@ -1,19 +1,110 @@
 package com.example.dnd5emanager.DataClasses;
 
 public class Spell {
-    String CastTime;
-    String[] Classes;
-    boolean Verbal;
-    boolean Somatic;
-    boolean Material;
-    String MaterialCost;
-    String Description;
-    String Duration;
-    int Level;
-    String Name;
-    String Range;
-    boolean Ritual;
-    String School;
+    private String CastTime = "";
+    public void setCastTime(String CastTime) {
+        this.CastTime = CastTime;
+    }
+    public String getCastTime() {
+        return CastTime;
+    }
+
+    private String[] Classes;
+    public void setClasses(String[] Classes) {
+        this.Classes = Classes;
+    }
+    public String[] getClasses() {
+        return Classes;
+    }
+
+    private boolean Verbal;
+    public void setVerbal(boolean Verbal) {
+        this.Verbal = Verbal;
+    }
+    public boolean getVerbal() {
+        return Verbal;
+    }
+
+    private boolean Somatic;
+    public void setSomatic(boolean Somatic) {
+        this.Somatic = Somatic;
+    }
+    public boolean getSomatic() {
+        return Somatic;
+    }
+
+    private boolean Material;
+    public void setMaterial(boolean Material) {
+        this.Material = Material;
+    }
+    public boolean getMaterial() {
+        return Material;
+    }
+
+    private String MaterialCost;
+
+    public void setMaterialCost(String MaterialCost) {
+        this.MaterialCost = MaterialCost;
+    }
+    public String getMaterialCost() {
+        return MaterialCost;
+    }
+
+    private String Description;
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+    public String getDescription() {
+        return Description;
+    }
+
+    private String Duration;
+    public void setDuration(String Duration) {
+        this.Duration = Duration;
+    }
+    public String getDuration() {
+        return Duration;
+    }
+
+    private int Level;
+    public void setLevel(int Level) {
+        this.Level = Level;
+    }
+    public int getLevel() {
+        return Level;
+    }
+
+    private String Name;
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    private String Range;
+    public void setRange(String Range) {
+        this.Range = Range;
+    }
+    public String getRange() {
+        return Range;
+    }
+
+    private boolean Ritual;
+    public void setRitual(boolean Ritual) {
+        this.Ritual = Ritual;
+    }
+    public boolean getRitual() {
+        return Ritual;
+    }
+
+    private String School;
+    public void setSchool(String School) {
+        this.School = School;
+    }
+    public String getSchool() {
+        return School;
+    }
 
     public Spell(String CastTime, String[] Classes, boolean Verbal, boolean Somatic, boolean Material,
     String MaterialCost, String Description, String Duration, int Level, String Name, String Range,
--- app/src/main/java/com/example/dnd5emanager/spellsInformation.java
+++ app/src/main/java/com/example/dnd5emanager/spellsInformation.java
@@ -1,16 +1,24 @@
 package com.example.dnd5emanager;
 
+import static com.example.dnd5emanager.DataClasses.Constants.Spells;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.example.dnd5emanager.DataClasses.Spell;
 import com.example.dnd5emanager.databinding.SpellsInformationBinding;
+
+import java.util.Arrays;
 
 
 public class spellsInformation extends Fragment {
@@ -31,6 +39,46 @@ public class spellsInformation extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String[] SpellNames = Spells.keySet().toArray(new String[0]);
+        Arrays.sort(SpellNames);
+
+        Spinner SpellSelected = view.findViewById(R.id.spellSelector);
+        ArrayAdapter<String> spellAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, SpellNames);
+        spellAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpellSelected.setAdapter(spellAdapter);
+
+        TextView CastingTime = view.findViewById(R.id.spellCastingTime);
+        TextView Range = view.findViewById(R.id.spellRange);
+        TextView Description = view.findViewById(R.id.SpellDescription);
+        TextView SupportedClasses = view.findViewById(R.id.supportedClasses);
+        TextView Components = view.findViewById(R.id.spellComponents);
+
+        SpellSelected.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spell SelectedSpell = Spells.get(SpellSelected.getSelectedItem().toString());
+                if (CastingTime != null) {
+                    CastingTime.setText(SelectedSpell.getCastTime());
+                }
+                if (Range != null) {
+                    Range.setText(SelectedSpell.getRange());
+                }
+                if (Description != null) {
+                    Description.setText(SelectedSpell.getDescription());
+                }
+                if (SupportedClasses != null) {
+                    SupportedClasses.setText(Arrays.toString(SelectedSpell.getClasses()));
+                }
+                if (Components != null) {
+                    Components.setText(SelectedSpell.getMaterialCost());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         binding.backtodataview.setOnClickListener(v ->
                 NavHostFragment.findNavController(spellsInformation.this).navigate(R.id.goToData));
